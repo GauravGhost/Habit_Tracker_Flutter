9dc2e2ac6989f7d37afe5d9373d6648ee64aa053
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-04-20 17:28:45 +0530

    Added create habit button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,76 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/themes/theme_provider.dart';
+import 'package:habit_tracker/components/my_drawer.dart';
+import 'package:habit_tracker/database/habit_database.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // text controller
+  final TextEditingController textController = TextEditingController();
+
+  // create new habit
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(
+            hintText: "Create a new habit",
+          ),
+        ),
+        actions: [
+          // Save Button
+          MaterialButton(
+            onPressed: () {
+              // get the habit name
+              String newHabitName = textController.text;
+
+              // save to db
+              context.read<HabitDatabase>().addHabit(newHabitName);
+
+              // pop box
+              Navigator.pop(context);
+
+              // clear controller
+              textController.clear();
+            },
+            child: const Text("Save"),
+          ),
+
+          // Cancel Button
+          MaterialButton(
+            onPressed: () {
+              // pop box
+              Navigator.pop(context);
+
+              // clear controller
+              textController.clear();
+            },
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) =>
-                Provider.of<ThemeProvider>(context, listen: false).toggleTheme(),
-          ),
-        ),
-      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      drawer: const MyDrawer(),
       appBar: AppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
